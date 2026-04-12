d987d015a2047f7c3f3401faffb5d152fe5c69b7
tascagio <giovanni.tasca23@gmail.com>
2026-04-12 18:15:29 -0300

    Add files via upload

---
+++ public/config.js
@@ -0,0 +1,16 @@
+window.FINANCEHERO_CONFIG = {
+  siteUrl: "https://financehero.com.br",
+  calendlyUrl: "https://calendly.com/giovanni-financehero/30min",
+  whatsappUrl: "",
+  contactEmail: "giovanni@financehero.com.br",
+  consentVersion: "2026-04",
+  analyticsEnabled: true,
+  supabase: {
+    url: "",
+    anonKey: "",
+    functions: {
+      captureLead: "capture-lead",
+      trackEvent: "track-event"
+    }
+  }
+};
